b0859efffa0b89b37b73c5d13c0b9818d4b698e0
RODRIGO DE LA FUENTE <rodridelafuente@uc.cl>
2023-04-05 23:50:22 -0400

    hello world

---
+++ app/models/bienvenido.rb
@@ -0,0 +1,2 @@
+class Bienvenido < ApplicationRecord
+end
---
+++ test/models/bienvenido_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BienvenidoTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
